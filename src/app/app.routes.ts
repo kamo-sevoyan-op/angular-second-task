ac8f1dd538b1558137d14083505372caa408b689
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-21 13:52:47 +0400

    Add route structure to the app.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { MainpageComponent } from './pages/main/main.component';
+import { RulesEngineComponent } from './components/rules-engine/rules-engine.component';
+import { NotImplementedComponent } from './components/not-implemented/not-implemented.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainpageComponent,
+    children: [
+      {
+        path: 'routes-engine',
+        component: RulesEngineComponent,
+      },
+      {
+        path: '**',
+        component: NotImplementedComponent,
+      },
+    ],
+  },
+];
