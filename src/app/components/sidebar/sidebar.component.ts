cbdda44b57ef600ca693ebc0320c0a5916b75a77
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-21 13:53:55 +0400

    Add sidebar component.

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav } from '@angular/material/sidenav';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [MatSidenavModule, MatSidenav, RouterModule, RouterOutlet],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css',
+})
+export class SidebarComponent {
+  navigations = [
+    { name: 'Dashboard', link: '' },
+    { name: 'Rules Engine', link: '/routes-engine' },
+    { name: 'Accounts', link: '' },
+    { name: 'Billing', link: '' },
+    { name: 'User Management', link: '' },
+    { name: 'Settings', link: '' },
+  ];
+}
