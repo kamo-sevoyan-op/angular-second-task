1fc4e5af3117eb68d481153865a5675ea77a2427
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-21 13:54:19 +0400

    Add main page component.

---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [SidebarComponent, NavbarComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainpageComponent {
+
+}
