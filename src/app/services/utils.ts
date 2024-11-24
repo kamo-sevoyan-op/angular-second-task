83eeba7a71e9039cf320cb6da8c03a5dbda965ba
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-25 00:20:04 +0400

    Format code.

--- src/app/services/utils.ts
+++ src/app/services/utils.ts
@@ -1,3 +1,3 @@
-export function getRandomIndex(length: number){
-    return Math.floor(Math.random() * length);
+export function getRandomIndex(length: number) {
+  return Math.floor(Math.random() * length);
 }
