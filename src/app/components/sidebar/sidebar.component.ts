1abb23238a862d6bda12b0678410a5b4811bea8a
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-28 21:00:55 +0400

    Change sidebar style. Now it has mini version.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,25 +1,60 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSidenav } from '@angular/material/sidenav';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { MatIconRegistry, MatIconModule } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatSidenavModule, MatSidenav, RouterModule, RouterOutlet],
+  imports: [
+    MatSidenavModule,
+    MatSidenav,
+    RouterModule,
+    RouterOutlet,
+    MatListModule,
+    MatIconModule,
+    MatTooltipModule,
+    CommonModule,
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
-
   toggled = input.required<boolean>();
-  
+
   navigations = [
-    { name: 'Dashboard', link: '' },
-    { name: 'Rules Engine', link: '/rules-engine' },
-    { name: 'Accounts', link: '' },
-    { name: 'Billing', link: '' },
-    { name: 'User Management', link: '' },
-    { name: 'Settings', link: '' },
+    { name: 'Dashboard', link: '', iconName: 'app-dashboard-icon' },
+    {
+      name: 'Rules Engine',
+      link: '/rules-engine',
+      iconName: 'app-rules_engine-icon',
+    },
+    { name: 'Accounts', link: '', iconName: 'app-accounts-icon' },
+    { name: 'Billing', link: '', iconName: 'app-billing-icon' },
+    {
+      name: 'User Management',
+      link: '',
+      iconName: 'app-user_management-icon',
+    },
+    { name: 'Settings', link: '', iconName: 'app-settings-icon' },
   ];
+
+  constructor() {
+    const iconRegistry = inject(MatIconRegistry);
+    const sanitizer = inject(DomSanitizer);
+
+    for (const navigation of this.navigations) {
+      const url = `/icons/${navigation.iconName}.svg`;
+      console.log(url);
+      iconRegistry.addSvgIcon(
+        navigation.iconName,
+        sanitizer.bypassSecurityTrustResourceUrl(url)
+      );
+    }
+  }
 }
