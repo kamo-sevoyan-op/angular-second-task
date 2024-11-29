b956b0ee2739bdc9aa3a6d13b6d1e19bddc0e927
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-29 18:30:52 +0400

    Add bell icon besides the dummy avatar.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
-import { Component, output } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
+import { MatIcon, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navbar',
@@ -14,5 +15,17 @@ export class NavbarComponent {
   menuClicked = output<void>();
   onMenuClicked() {
     this.menuClicked.emit();
+  }
+
+  constructor() {
+    const iconRegistry = inject(MatIconRegistry);
+    const sanitizer = inject(DomSanitizer);
+
+    const name = 'app-bell-icon';
+    const url = `/icons/${name}.svg`;
+    iconRegistry.addSvgIcon(
+      name,
+      sanitizer.bypassSecurityTrustResourceUrl(url)
+    );
   }
 }
